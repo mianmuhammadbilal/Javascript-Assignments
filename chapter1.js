1a9b33bbcd2378cd326839c674054ea104492161
mianmuhammadbilal <xtylishweb@gmail.com>
2025-04-09 13:13:21 +0500

    intial commit

---
+++ chapter1.js
@@ -0,0 +1,10 @@
+
+
+        alert("Welcome to javascript");
+        alert("Erorr! Please enter valid password.");
+        alert("Welcome to JS Land...\n Happy Coding!");
+        alert("Welcome to JS Land..");
+        alert("Happy Coding");
+        console.log("Hello... I can run JS through my browser's console");
+        
+
---
+++ chapter2.js
@@ -0,0 +1,26 @@
+
+        var username;
+        var myName="Muhammd Bilal";
+        var titledmsg;
+        titledmsg="Hello World!";
+        alert(titledmsg);
+        var student="Bilal";
+        var age=23;
+        var course="Certified web & mobile application development";
+        alert(student);
+        alert(age);
+        alert(course);
+        var pizza="Pizza";
+        alert(pizza.slice(0,5) +"\n"+ pizza.slice(0,4) +"\n"+ pizza.slice(0,3) +"\n"+ pizza.slice(0,2) +"\n"+ pizza.slice(0,1));
+        var email="xtylishweb@gmail.com";
+        alert("My Email address is:" +"  " + email);
+        var book="A smarter way to learn javascript";
+        alert("I am trying to learn from the book "+book);
+        document.write("Yeah! I can write HTML content through Javascript.<br>");
+        var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+        alert(design);
+        document.write(design);
+
+
+
+  
